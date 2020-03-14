f95883e9fdbbeb67d0f51e759c25f7e39e55811f
Andrii Sabinin <kaboomspeed@gmail.com>
2020-03-14 03:02:57 +0200

    Created base class for calculations Created strategy to use formulas Basic pattern for send tokens and keys

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+class CardInfo():
+
+    card_number = ''
+    card_cvv = ''
+    card_due_date = ''
+
+
+class User():
+
+    def sendCardInfo(model):
+        pass
+
+    def encryptCardInfo(rsa, exp):
+        pass
+
+
+class Server():
+
+
+
+class Internet():
+
+    def transfer():
+        pass
+
+
+class Hacker():
+
+    def hack():
+        pass
