2ee5c2f0ea8a4e7f172a66302cdd7b33263e8bab
Andrii Sabinin <kaboomspeed@gmail.com>
2020-03-21 23:41:29 +0200

    Added new function to encrypt text message into numbers Global refactor

--- app/main.py
+++ app/main.py
@@ -1,28 +1,51 @@
-import secret_exp
+from pythonRSA import *
 
-crypto = secret_exp.Crypto()
-array = crypto.buildPrimeNumbers(200)
-number = crypto.getRandomNumber(20, len(array)-1, False)
+
+
+crypto = RSAHelper()
+array = crypto.buildPrimeNumbers(1000)
+number = crypto.getRandomNumber(100, len(array)-1, False)
 A = array[number]
-print("A: " + str(A))
-number = crypto.getRandomNumber(40, len(array)-1, False)
-B = array[number]
-print("B: " + str(B))
+B = A
+while A == B:
+    number = crypto.getRandomNumber(100, len(array)-1, False)
+    B = array[number]
+
+if B == A:
+    print("ErrorCode: 12 A shoud't be B")
+    quit()
 
 N = crypto.generateRSAKey(A, B)
-print("N: " + str(N))
 fi = crypto.eilerFunction(A, B)
-print("fi: " + str(fi))
 e = crypto.findExp(fi, array)
-print("e: " + str(e))
 d = crypto.secretExp(fi, e)
-print("d: " + str(d))
 
-message = 721
-print('message: ' + str(message))
+print('A' + 'B' + 'N' + 'fi' + 'e' + 'd')
 
-secretmessage = crypto.encrypt(e, N, message)
-print('secretmessage: ' + str(secretmessage))
 
-decryptedmessage = crypto.decrypt(d, N, secretmessage)
-print('decryptedmessage: ' + str(decryptedmessage))
+def encryptMessage(message, e, N):
+
+    list_encrypted_numbers = []
+    for ch in message:
+        number = ord(ch)
+        encrypted_number = crypto.encrypt(e, N, number)
+        list_encrypted_numbers.append(encrypted_number)
+    return list_encrypted_numbers
+
+
+def decryptMessage(list_encrypted_numbers, d, N):
+    list_characters = []
+    for number in list_encrypted_numbers:
+        encrypted_number = crypto.decrypt(d, N, number)
+        char = chr(encrypted_number)
+
+        list_characters.append(char)
+    return list_characters
+
+
+message = "Hello encrypt!"
+list_encrypted = encryptMessage(message, e, N)
+print(list_encrypted)
+
+list_characters = decryptMessage(list_encrypted, d, N)
+print(list_characters)
