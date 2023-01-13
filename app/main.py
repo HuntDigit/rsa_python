1e785e870db2d4a0f08400f7a3b584f01d766aa4
Андрій Сабінін <kaboomspeed@gmail.com>
2023-01-13 15:04:10 +0200

    Commit from web editor

--- app/main.py
+++ app/main.py
@@ -22,6 +22,8 @@ d = crypto.secretExp(fi, e)
 loger = 'A: \t{0} \nB: \t{1} \nN: \t{2} \nfi: \t{3} \ne: \t{4} \nd: \t{5}'.format(A, B, N, fi, e, d)
 print('\n---------------\n' + loger + '\n---------------\n')
 
+def emptyFunction():
+    return 0
 
 def encryptMessage(message, e, N):
 
